e3cb4390ebb5de6221757ccdae628a7ea423292f
Akshansh Bhanjana <akshansh2000@gmail.com>
2020-05-05 13:35:10 +0530

    trying to mail

--- forms/contact.php
+++ forms/contact.php
@@ -9,19 +9,19 @@
   // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'akshansh2000@gmail.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+  // if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+  //   include( $php_email_form );
+  // } else {
+  //   die( 'Unable to load the "PHP Email Form" Library!');
+  // }
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
+  // $contact = new PHP_Email_Form;
+  // $contact->ajax = true;
   
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+  // $contact->to = $receiving_email_address;
+  // $contact->from_name = $_POST['name'];
+  // $contact->from_email = $_POST['email'];
+  // $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
@@ -33,9 +33,18 @@
   );
   */
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+  // $contact->add_message( $_POST['name'], 'From');
+  // $contact->add_message( $_POST['email'], 'Email');
+  // $contact->add_message( $_POST['message'], 'Message', 10);
 
-  echo $contact->send();
+  // echo $contact->send();
+
+  $message = $_POST['message'];
+  $message = "Name: ".$POST['name']."\n\n";
+
+  $headers = 'From: '.$_POST['email']."\r\n".
+  'Reply-To: '.$POST['email']."\r\n".
+  'X-Mailer: PHP/'.phpversion();
+
+  mail($receiving_email_address, $_POST['subject'], $message, $headers);
 ?>
